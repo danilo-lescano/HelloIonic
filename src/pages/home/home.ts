4d35dc78e2280dc83291ddef5be4dd8e1e27b1fe
danilocaverna <nq3i4fsx@hotmail.com>
2019-01-20 17:21:46 -0300

    ts n aceita variaveis nulas ¯\_(ツ)_/¯

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,21 +19,21 @@ export class HomePage {
 	}
 	promptPartyName() {
 		let alert = this.alertCtrl.create({
-			title: this.msg.partyName,
+			title: this.msg["partyName"],
 			inputs: [
 				{
 					name: 'partyName',
-					placeholder: this.msg.partyName
+					placeholder: this.msg["partyName"],
 				},
 			],
 			buttons: [
 				{
-					text: this.msg.cancel,
+					text: this.msg["cancel"],
 					role: 'cancel',
 					handler: data => {}
 				},
 				{
-					text: this.msg.ok,
+					text: this.msg["ok"],
 					handler: data => {
 						this.newParty(data.partyName);
 					}
