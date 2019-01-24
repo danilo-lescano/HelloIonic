c6cc643935fc671c27ed1944645111f4004a9574
danilocaverna <nq3i4fsx@hotmail.com>
2019-01-24 17:04:30 -0300

    bestiary crawler++ refinamento wip

--- Crawler/refinamento_1.js
+++ Crawler/refinamento_1.js
@@ -2,6 +2,8 @@ var https = require('https');
 var fs = require('fs');
 
 var lista;
+var newList = [];
+var defeito = [];
 load();
 function load(){
     fs.readFile('bestiary_body.json', function(err, data) {
@@ -9,20 +11,55 @@ function load(){
         start();
     });
 }
+var erA = 0, erH = 0, erI = 0;
 function start(){
     var cont = 0;
+    var err = 0;
     for (let i = 0; i < lista.length; i++) {
-        /*if(lista[i].description != ""){
-            var aux = lista[i].description.split(">")[1].split("<")[0];
-            if(aux == "")
-                console.log(lista[i].description);
-        }*/
+        if(lista[i].statblock != ""){
+            let init, ac, hp;
+            try {
+                init = lista[i].statblock.toLowerCase().split("<b>init")[1].match(/\d+/)[0];
+            } catch (error) {erI++;}
+            try {
+                ac = lista[i].statblock.toLowerCase().split("<b>ac")[1].match(/\d+/)[0];
+            } catch (error) {erA++;}
+            try {
+                hp = lista[i].statblock.toLowerCase().split("hp")[1].match(/\(.*\)/)[0];
+            } catch (error) {erH++;}
 
-        if(lista[i].description != "" && lista[i].statblock != "")
-            cont++;
-        else
-            console.log(lista[i].name);
-        
+            if(init && ac && hp){
+                newList[newList.length] = {
+                    name: lista[i].name,
+                    ac: ac,
+                    hp: hp,
+                    init: init
+                };
+                cont++;
+            }
+            else{
+                defeito[defeito.length] = lista[i];
+                err++;
+            }
+        }
+
     }
-    console.log(cont);
+    console.log("total: " + lista.length);
+    console.log("cont: " + cont);
+    console.log("erros: " + err);
+    for (let i = 0; i < array.length; i++)
+        console.log(defeito[i].name);
+    
+    //save();
+}
+
+function save(){
+    fs.writeFile('bestiary_refinado_1.json', JSON.stringify(newList), function(err, data) {
+        if (err) throw err;
+        console.log('Saved newList!');
+    });
+    fs.writeFile('bestiary_erro_1.json', JSON.stringify(defeito), function(err, data) {
+        if (err) throw err;
+        console.log('Saved defeito!');
+    });
 }
